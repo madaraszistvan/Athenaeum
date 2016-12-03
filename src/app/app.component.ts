901733b19bfdd2cd07400e5e1db731436c69a810
myrluk3 <madarasz.pista@gmail.com>
2016-12-03 22:13:17 +0200

    [boostrap] adding bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css',
+    '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+  ]
 })
 export class AppComponent {
-  title = 'app works hehe!';
+  title = 'app works!';
 }
