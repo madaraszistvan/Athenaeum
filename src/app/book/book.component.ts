ded5d966b8640ddc67a3d0141c82206f13f55324
myrluk3 <madarasz.pista@gmail.com>
2016-12-04 01:03:02 +0200

    [sass] adding sass

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css']
+  styleUrls: ['book.component.scss']
 })
 export class BookComponent implements OnInit {
 
